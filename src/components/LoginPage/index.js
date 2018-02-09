f82eb5798d24c5a08806883114d8433cc9c402db
gustavogodoi <gugodoi@gmail.com>
2018-02-09 11:21:09 -0200

    LoginPage styled

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import './LoginPage.css';
 
 class LoginPage extends Component {
   render() {
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="login-wrapper">
+        <form className="login-form">
+          <div className="login-description">
+            Efetue seu login para acessar o sistema
+          </div>
+          <div className="login-form__body">
+            <div className="login-fields">
+              <label className="login-label" htmlFor="login">
+                Login:
+              </label>
+              <input className="login-input" name="login" type="text" />
+            </div>
+            <div className="login-fields">
+              <label className="login-label" htmlFor="password">
+                Password:
+              </label>
+              <input className="login-input" name="password" type="password" />
+            </div>
+            <div className="login-action">
+              <button className="login-button">Nova Conta</button>
+              <button className="login-button">Login</button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
