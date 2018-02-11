b9ceaa7fddb3b4464a5e0cfbce95eebd82be922f
Gustavo Godoi <gugodoi@gmail.com>
2018-02-11 18:06:36 -0200

    'Meus Pedidos' page styled

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,7 @@ import DashboardPage from '../DashboardPage';
 import MyAccountPage from '../MyAccountPage';
 import NewCompanyPage from '../NewCompanyPage';
 import NewOrderPage from '../NewOrderPage';
+import MyOrdersPage from '../MyOrdersPage';
 import LoginPage from '../LoginPage';
 
 class App extends Component {
@@ -13,7 +14,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/dashboard" component={DashboardPage} />
-          <Route exact path="/meus-pedidos/:id" component={DashboardPage} />
+          <Route exact path="/meus-pedidos/:id" component={MyOrdersPage} />
           <Route exact path="/nova-empresa" component={NewCompanyPage} />
           <Route exact path="/novo-pedido" component={NewOrderPage} />
           <Route exact path="/minha-conta" component={MyAccountPage} />
